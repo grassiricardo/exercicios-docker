ff1ff24801fa6c00144f722cc8f2564a77f825ff
grassiricardo <grassiricardorg@gmail.com>
2017-09-17 22:41:50 -0300

    mult workers

--- email-worker-compose/worker/worker.py
+++ email-worker-compose/worker/worker.py
@@ -4,7 +4,8 @@ from time import sleep
 from random import randint
 
 if __name__ == '__main__':
-    r = redis.Redis(host='queue', port(6379), db=0)
+    r = redis.Redis(host='queue', port=(6379), db=0)
+    print('Aguardando mensagens ...')
     while True:
         mensagem = json.loads(r.blpop('sender')[1])
         # Simulando envio de e-mail...
